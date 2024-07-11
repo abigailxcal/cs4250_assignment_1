a0469cd2c9d2167af49e1a4ea0c25c159903bc03
abigailxcal <acalde1997@gmail.com>
2024-07-10 21:28:34 -0700

    cleaned up code, formatted the doc-term matrix so that the actual index terms show up as columns

--- indexing.py
+++ indexing.py
@@ -21,14 +21,13 @@ with open('collection.csv', 'r') as csvfile:
         if i > 0: # skipping the header
             documents.append(row[0])
 
+
 def print_docs(D):
     for index,D in enumerate(D):
         print(f'\tDocument {index+1}: {D}')
 
 # for each document, turn it's string into a list and put into a container of all documents 
-docs =[]
-for line in documents:
-    docs.append(line.split())
+docs = [line.split() for line in documents]
 
 print('Original Document Contents:')
 print_docs(docs)
@@ -49,24 +48,22 @@ print_docs(docs)
 
 
 #Conducting stemming. Hint: use a dictionary to map word variations to their stem.
-#--> add your Python code here
-stemming = {'lov': ['loved','loves', 'lover', 'loving','love'],
+stemming = {'love': ['loved','loves', 'lover', 'loving','love'],
             'cat': ['cats'],
             'dog': ['dogs','doggie','doggy','doggies']
-            }
+}
 
 for i in range(len(docs)):
     mylist = []
     for j in range(len(docs[i])):
-        for k,v in stemming.items():
-            if k in docs[i][j].lower() and docs[i][j].lower() in v: 
-                docs[i][j]=k
+        for stem, variations in stemming.items():
+            if docs[i][j].lower() in variations:
+                docs[i][j] = stem
 print(f'Document contents after stemming: ')
 print_docs(docs)
 
 
 #Identifying the index terms.
-#--> add your Python code here
 terms = []
 for doc in docs:
     for word in doc:
@@ -76,40 +73,26 @@ print(f'Terms: {terms}')
 
 # tf(t,d) is the relative frequency of term t within document d
 def tf(term,d):
-    total_terms = len(d)
-    freq_count = 0
-    for word in d:
-        if word == term:
-            freq_count+=1
-    #print(f'Total terms within doc: {total_terms}\n"{term}" count: {freq_count}')
-    #print(f'tf: {freq_count/total_terms} for {term} in {d}')
-    return freq_count/total_terms
+    return d.count(term)/len(d)
 
 
 #df(t, D), is the number of documents in the document set D in which the term t appears
 def df(term,D):
-    total_doc_count = len(D)
     term_count = 0
     for doc in D:
         if term in doc:
-            #print(f'Term: {term} found in {doc}')
             term_count +=1 
-    #print("df: ",term_count, "for term ",term) 
     return term_count
 
 #idf(t, D), is the logarithmically (base 10) scaled inverse fraction of the documents D that contain the term t
 def idf(term,D):
-    a = math.log10( (len(D))/df(term,D) )
-    #print("idf: ",a, "for term ",term)
-    return round(a,2)
+    return round(math.log10( (len(D))/df(term,D) ),2)
 
 def tf_idf(term,d,D):
-    #print("tf-idf: ",tf(term,d)*idf(term,D), " for term ",term, " and doc ",d)
     return tf(term,d)*idf(term,D)
 
 
 #Building the document-term matrix by using the tf-idf weights.
-#--> add your Python code here
 docTermMatrix = []
 for doc in docs:
     term_count = []
@@ -119,10 +102,9 @@ for doc in docs:
 
 
 #Printing the document-term matrix.
-#--> add your Python code here
 num = 1
 column_width = 10
-print(f"{'Doc-Term Matrix':<{column_width}} | {'Term 1':<{column_width}} | {'Term 2':<{column_width}} | {'Term 3':<{column_width}} |")
+print(f"{'Doc-Term Matrix':<{column_width}} | " + " | ".join(f"{term:<{column_width}}" for term in terms) + " |")
 print("-" * (column_width * 5 + 16))
 for r in range(len(docTermMatrix)):
     print(f"{'     Document ' + str(num):<{column_width}} | ", end="")
@@ -131,11 +113,4 @@ for r in range(len(docTermMatrix)):
     print("")
     num+=1
 
-# tf_matrix = []
-# for d in docs:
-#     myList = []
-#     for t in terms:
-#         #myList.append(tf(t,d))
-#         #myList.append(df(t,docs))
-#         myList.append(idf(t,docs))
-#     #print(myList)
+
